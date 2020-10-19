132b3c6cd55379e3962268ed2ca7d78bc958c79a
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-19 20:54:09 +0530

    Added contact and about page

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ app.get("/", (req, res) =>
     res.send("<h1>Hello world!</h1>");
 });
 
+app.get("/contact", (req, res) =>
+{
+    res.send("Contact me at: devdeveshsingh@gmail.com");
+});
+
+app.get("/about", (req, res) =>
+{
+    res.send("My name is Devesh Kumar Singh, and I am currently learning web development. I have created this local web server just for practicing.");
+});
+
 app.listen(3000, () =>
 {
     console.log("Server has started on port 3000");
